977deb0d3125660de7ad44b9e4ef367a6ae14cfe
Elvir Tabakovic <elvir.tabakovic.92@gmail.com>
2022-08-22 00:47:19 +0200

    feat: using color picker in playground app

--- projects/playground/src/app/pages/home/home.component.ts
+++ projects/playground/src/app/pages/home/home.component.ts
@@ -1,14 +1,17 @@
+import { IColorIndicator } from 'projects/color-indicator-widget/src/public-api';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class HomeComponent{
+  colors: IColorIndicator[] = [
+    { color: 'red', width: '10%' },
+    { color: 'black', width: '30%' },
+    { color: 'blue', width: '10%' },
+    { color: '#fcba03', width: '10%' },
+    { color: 'yellow', width: '40%' },
+  ];
 
 }
